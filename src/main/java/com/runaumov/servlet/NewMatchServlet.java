0003a3dd033f8c6fa5ddd5de7151d81f6c6ae08e
Runaumov <casrunau@gmail.com>
2024-10-14 21:36:03 +0300

    Create RequestNewMatchDto

--- src/main/java/com/runaumov/servlet/NewMatchServlet.java
+++ src/main/java/com/runaumov/servlet/NewMatchServlet.java
@@ -1,5 +1,6 @@
 package com.runaumov.servlet;
 
+import com.runaumov.dto.RequestNewMatchDto;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
@@ -15,5 +16,11 @@ public class NewMatchServlet extends HttpServlet {
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String palyer1Name = req.getParameter("player1Name");
         String palyer2Name = req.getParameter("player2Name");
+
+        //TODO: реализовать валидацию входных данных
+
+        RequestNewMatchDto requestNewMatchDto = new RequestNewMatchDto(palyer1Name, palyer2Name);
+
+
     }
 }
