79d6467a902ff9b1578b173c9a4cc1dd21c3a64c
Runaumov <casrunau@gmail.com>
2024-11-25 23:10:32 +0300

    refactor: MatchResultService

--- src/main/java/com/runaumov/service/status/MatchResultService.java
+++ src/main/java/com/runaumov/service/status/MatchResultService.java
@@ -2,25 +2,29 @@ package com.runaumov.service.status;
 
 import com.runaumov.entity.Match;
 import com.runaumov.entity.Player;
+import com.runaumov.exception.ScoreUpdateException;
+import com.runaumov.model.MatchScore;
 
 public class MatchResultService {
     private static final int WINNING_SET_COUNT = 2;
 
     public Player getWinner(Match match) {
-        Player player1Id = match.getPlayer1Id();
-        Player player2Id = match.getPlayer2Id();
+        MatchScore matchScore = match.getMatchScore();
+        Player playerOne = match.getPlayer1Id();
+        Player playerTwo = match.getPlayer2Id();
 
-        // TODO : всрато выглядит, переделать
-        if (match.getMatchScore().getSetScorePlayer1() == WINNING_SET_COUNT) {
-            setWinner(match, player1Id);
-            return player1Id;
-        } else if (match.getMatchScore().getSetScorePlayer2() == WINNING_SET_COUNT) {
-            setWinner(match, player2Id);
-            return player2Id;
+        if (matchScore.getSetScorePlayer1() == WINNING_SET_COUNT) {
+            return determinePlayer(match, playerOne);
+        } else if (matchScore.getSetScorePlayer2() == WINNING_SET_COUNT) {
+            return determinePlayer(match, playerTwo);
         } else {
-        // TODO : доделать
-        throw new IllegalArgumentException("Ошибка");
+            throw new ScoreUpdateException("Error for get winner");
         }
+    }
+
+    private Player determinePlayer(Match match, Player winner) {
+        setWinner(match, winner);
+        return winner;
     }
 
     private void setWinner(Match match, Player winner) {
