2a98703428591fc840b675d865db560c242222f9
Runaumov <casrunau@gmail.com>
2024-11-25 23:15:02 +0300

    refactor: MatchStatusChecker

--- src/main/java/com/runaumov/service/status/MatchStatusChecker.java
+++ src/main/java/com/runaumov/service/status/MatchStatusChecker.java
@@ -15,30 +15,27 @@ public class MatchStatusChecker {
     }
 
     public boolean isGameWin(Match match) {
-        String pointScore1 = match.getMatchScore().getPointScorePlayer1();
-        String pointScore2 = match.getMatchScore().getPointScorePlayer2();
+        String pointScoreForPlayerOne = match.getMatchScore().getPointScorePlayer1();
+        String pointScoreForPlayerTwo = match.getMatchScore().getPointScorePlayer2();
 
-        return (pointScore1.toUpperCase().equals(PointScore.WIN.name()) ||
-                pointScore2.toUpperCase().equals(PointScore.WIN.name()));
+        return (pointScoreForPlayerOne.toUpperCase().equals(PointScore.WIN.name()) ||
+                pointScoreForPlayerTwo.toUpperCase().equals(PointScore.WIN.name()));
     }
 
     // TODO : Возможно тут баг в условии. Надо чекнуть условия завершения сета
     public boolean isSetWin(Match match) {
-        int gameScorePlayer1 = match.getMatchScore().getGameScorePlayer1();
-        int gameScorePlayer2 = match.getMatchScore().getGameScorePlayer2();
-        int gameDifference = Math.abs(gameScorePlayer1 - gameScorePlayer2);
+        int gameScorePlayerOne = match.getMatchScore().getGameScorePlayer1();
+        int gameScorePlayerTwo = match.getMatchScore().getGameScorePlayer2();
+        int gameScoreDifference = Math.abs(gameScorePlayerOne - gameScorePlayerTwo);
 
-        if (gameDifference >= GAME_ADVANTAGE_DIFFERENCE &&
-                (gameScorePlayer1 >= MIN_WIN_GAMES || gameScorePlayer2 >= MIN_WIN_GAMES)
-        ) {
-            return true;
-        }
-        return false;
+        return  gameScoreDifference >= GAME_ADVANTAGE_DIFFERENCE &&
+                (gameScorePlayerOne >= MIN_WIN_GAMES || gameScorePlayerTwo >= MIN_WIN_GAMES);
     }
 
     public boolean isMatchWin(Match match) {
         int setScoreForPlayer1 = match.getMatchScore().getSetScorePlayer1();
         int setScoreForPlayer2 = match.getMatchScore().getSetScorePlayer2();
+
         return setScoreForPlayer1 == WINNING_SET_COUNT || setScoreForPlayer2 == WINNING_SET_COUNT;
     }
 }
